0fd5de394a6db7b4cb8cb6094d2c62e3ea873259
Godmod13 <s1874@mail.ru>
2017-08-01 11:10:55 +0500

    resolution_try

--- fixture/motion.py
+++ fixture/motion.py
@@ -8,11 +8,26 @@ class MotionHelper:
         self.app = app
 
 
+    def height_res(self):
+        driver = self.app.driver
+        windows_size = self.app.driver.get_window_size()
+        screen_height = windows_size.get('height')
+        return screen_height
+
+    def weight_res(self):
+        driver = self.app.driver
+        windows_size = self.app.driver.get_window_size()
+        screen_weight = windows_size.get('weight')
+        return screen_weight
+
     def swipe_left(self):
         driver = self.app.driver
+        startx = self.weight_res()
+        print(startx)
+        endx = None
+        starty = None
+        endy = None
         self.app.driver.swipe(800, 800, 100, 800,1000)
-
-
 
     def swipe_right(self):
         driver = self.app.driver
--- test/any_platform_test.py
+++ test/any_platform_test.py
@@ -2,16 +2,20 @@ import time
 
 
 #swipe News on dasboard
-def test_swipe_news(app):
-    time.sleep(1)
-    app.motion.swipe_left()
-    app.motion.swipe_left()
-    app.motion.swipe_left()
+# def test_swipe_news(app):
+#     time.sleep(1)
+#     app.motion.swipe_left()
+#     app.motion.swipe_left()
+#     app.motion.swipe_left()
+#
+#
+# def test_swipe_news_back(app):
+#     time.sleep(1)
+#     app.motion.swipe_right()
+#     app.motion.swipe_right()
 
 
-def test_swipe_news_back(app):
-    time.sleep(1)
-    app.motion.swipe_right()
-    app.motion.swipe_right()
 
+def test_windows_size(app):
+    app.motion.swipe_left()
 
